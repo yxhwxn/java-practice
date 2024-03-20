90a33cc1f36274a579ccc8b06fb8c0ffa2d7a0aa
yxhwxn <officeyh9819@gmail.com>
2024-03-20 21:58:06 +0900

    배열 선언 방식 3가지

--- src/array/Array1.java
+++ src/array/Array1.java
@@ -6,6 +6,9 @@ public class Array1 {
 
         int[] students;         //배열 변수 선언
         students = new int[5];  //배열의 주소값(참조값)만 가지고 있는 students에 int형 배열 생성 후 주입
+//        위 두 줄을 아래와 같이 쉽게 바꿀 수 있다.
+//        -> int[] students2 = new int[]{1, 2, 3, 4, 5};
+//        -> int[] students3 = {1, 2, 3, 4, 5}; (주로 사용하는 선언 방식)
 
         students[0] = 1;
 
