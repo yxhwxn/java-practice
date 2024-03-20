e59657f7c7e8609a65501167e2f4f852f40ea5d0
yxhwxn <officeyh9819@gmail.com>
2024-03-20 09:34:44 +0900

    배열 기본 선언

---
+++ src/array/Array1.java
@@ -0,0 +1,19 @@
+package array;
+
+public class Array1 {
+
+    public static void main(String[] args) {
+
+        int[] students;         //배열 변수 선언
+        students = new int[5];  //배열의 주소값(참조값)만 가지고 있는 students에 int형 배열 생성 후 주입
+
+        students[0] = 1;
+
+        /**
+         * java에서는 배열을 단순히 한 줄로 생성 하지 않고
+         * 배열 변수 선언  /  배열 생성       으로 나뉜다.
+         */
+    }
+
+
+}
